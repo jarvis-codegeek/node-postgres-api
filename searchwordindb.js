12bc1161ac297ddc335e872941a5cd3aff78d08e
shashankjoshi <shashankjoshi.cool@gmail.com>
2024-11-08 23:10:40 +0530

    indexing

--- searchwordindb.js
+++ searchwordindb.js
@@ -9,16 +9,31 @@ const client = new Pool({
     port: 5432,
 });
 
+function cleanString(input) {
+    // Remove special characters, pipelines, colons, parentheses, and brackets
+    let cleaned = input.replace(/[^a-zA-Z0-9\s]|[:|(){}\[\]]/g, '');
+
+    // Remove extra spaces
+    cleaned = cleaned.replace(/\s{2,}/g, ' ').trim();
+
+    return cleaned;
+}
+
+
 // Function to search for matching keywords in any order
 async function searchKeywords(keywords) {
+
+    const createIndexQuery = ` CREATE INDEX IF NOT EXISTS idx_tsvector ON dummyschema.cpetable USING GIN (to_tsvector('english', product_title)); `; 
     
+    await client.query(createIndexQuery);
+
     const query = `
         SELECT *
         FROM dummyschema.cpetable
-        WHERE to_tsvector('english', product_name) @@ to_tsquery($1)
+        WHERE to_tsvector('english', product_title) @@ to_tsquery($1)
     `;
 
-    const tsQuery = keywords.split(' ').join(' & '); // Join keywords with '&' for full-text search
+    const tsQuery = cleanString(keywords).split(' ').join(' & '); // Join keywords with '&' for full-text search
 
     try {
         const res = await client.query(query, [tsQuery]);
